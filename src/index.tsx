b0936713d9af5816de396a1d256d3e3ab91286bd
Rajesh <tipstricks118@gmail.com>
2024-05-23 09:45:13 +0545

    Fixed the type setting of Math Jax script

--- src/index.tsx
+++ src/index.tsx
@@ -7,40 +7,35 @@ declare global {
 }
 
 const Latex = ({ latexExpression }: { latexExpression: string | HTMLElement | number }) => {
-  const [_, setMathJax] = useState<Window["MathJax"] | null>(
-    window.MathJax || null
-  );
+  const [mathJax, setMathJax] = useState<Window["MathJax"] | null>(window.MathJax || null);
 
   useLayoutEffect(() => {
     const existingMathJaxScript = document.getElementById("MathJax-script");
 
     if (existingMathJaxScript) {
-      const onLoad = existingMathJaxScript.onload as () => void;
-      console.log("Latex",_)
-      window.MathJax?.typeset();
-
-      existingMathJaxScript.onload = () => {
-        onLoad();
+      // If MathJax script already exists, ensure MathJax is loaded and ready
+      if (window.MathJax) {
         setMathJax(window.MathJax);
-        window.MathJax?.typeset();
-      };
+        window.MathJax.typeset();
+      } else {
+        existingMathJaxScript.onload = () => {
+          setMathJax(window.MathJax);
+          window.MathJax.typeset();
+        };
+      }
     } else {
       const loadMathJax = async () => {
         try {
-          // @ts-ignore
-          await import("https://polyfill.io/v3/polyfill.min.js?features=es6");
           const script = document.createElement("script");
           script.id = "MathJax-script";
-          script.src =
-            "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js";
+          script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js";
           script.async = true;
           script.type = "text/javascript";
           document.head.appendChild(script);
 
           script.onload = () => {
             setMathJax(window.MathJax);
-            // Manually typeset after MathJax is loaded
-            window.MathJax?.typeset();
+            window.MathJax.typeset();
           };
         } catch (error) {
           console.error("Error loading MathJax:", error);
@@ -51,12 +46,16 @@ const Latex = ({ latexExpression }: { latexExpression: string | HTMLElement | nu
     }
 
     // Clean up effect
-    return () => {};
+    return () => {
+      if (existingMathJaxScript) {
+        existingMathJaxScript.onload = null;
+      }
+    };
   }, [latexExpression]); // Add latexExpression as a dependency to trigger the effect on changes
 
   return (
     <div
-      className="w-full"
+      className="w-full font-normal"
       dangerouslySetInnerHTML={{ __html: latexExpression }}
     />
   );
